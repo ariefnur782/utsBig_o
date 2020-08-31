ea066baa3cab329b4d1d90477769fd931e3eb151
ariefnur782 <54919802+ariefnur782@users.noreply.github.com>
2020-08-31 21:49:38 +0700

    Add files via upload

---
+++ Big_o/src/big_o/Big_o.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package big_o;
+
+import java.util.function.Function;
+import static jdk.nashorn.internal.objects.NativeFunction.function;
+import static jdk.nashorn.tools.ShellFunctions.input;
+
+/**
+ *
+ * @author USER
+ */
+public class Big_o {
+    public static void main(String[] args) {
+        int arr[] = {2,3,4};
+        
+    }
+    }
+
+    private sta var {
+
+        public var() {
+            
+        }
+    }
+
+
